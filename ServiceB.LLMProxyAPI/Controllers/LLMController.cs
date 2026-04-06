2f821925bdf5abe003af170adb489d8b34c82a9a
Abdalle <abdalle-abdulkadir@chasacademy.se>
2026-04-06 12:03:53 +0200

    setup project structure and basic controllers

---
+++ ServiceB.LLMProxyAPI/Controllers/LLMController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LLMController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult GenerateEmail()
+    {
+        return Ok("LLM response");
+    }
+}
+
